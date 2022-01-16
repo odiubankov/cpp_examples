7f78abd99c0cae7c7884c0ef34e488c0935c633b
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2022-01-16 17:45:35 +0100

    Structured bindings example

---
+++ 17/structured_bindings/structured_bindings.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+TEST(structured_bindings, structure) {
+    struct Point {
+        int x;
+        int y;
+    };
+
+    Point p = { 1,2 };
+    
+    auto[ x_coord, y_coord ] = p;
+
+    ASSERT_EQ(x_coord, 1);
+    ASSERT_EQ(y_coord, 2);
+}
