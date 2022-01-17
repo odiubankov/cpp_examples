41dcb3f9a126b88c91dea56d79a81e9b79d96041
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2022-01-17 16:11:30 +0100

    Enable structured binding for a class

---
+++ 17/structured_bindings/enable_structured_binding.cpp
@@ -0,0 +1,88 @@
+#include <string>
+#include <utility>
+
+#include "gtest/gtest.h"
+
+// Class with support of structured binding
+class Person {
+ public:
+  Person(std::string name, std::string surname, int age)
+      : name_{std::move(name)}, surname_{std::move(surname)}, age_{age} {}
+
+  const std::string& getName() const { return name_; }
+  const std::string& getSurname() const { return surname_; }
+  int getAge() const { return age_; }
+
+  std::string& getName() { return name_; }
+  std::string& getSurname() { return surname_; }
+  int& getAge() { return age_; }
+
+ private:
+  std::string name_;
+  std::string surname_;
+  int age_;
+};
+
+// Number of elements for binding
+template <>
+struct std::tuple_size<Person> {
+  static constexpr int value = 3;
+};
+
+// Type of the element with index 2
+template <>
+struct std::tuple_element<2, Person> {
+  using type = int;
+};
+
+// Type of other elements
+template <std::size_t Index>
+struct std::tuple_element<Index, Person> {
+  using type = std::string;
+};
+
+// Getters for corresponding elements
+template <std::size_t Index, typename PersonT>
+decltype(auto) get_person(PersonT&& person) {
+  static_assert(Index < std::tuple_size<Person>::value);
+
+  if constexpr (Index == 0)
+    return person.getName();
+  else if constexpr (Index == 1)
+    return person.getSurname();
+  else
+    return person.getAge();
+}
+
+template <std::size_t Index>
+decltype(auto) get(Person& person) {
+  return get_person<Index>(person);
+}
+
+template <std::size_t Index>
+decltype(auto) get(Person&& person) {
+  return get_person<Index>(std::move(person));
+}
+
+template <std::size_t Index>
+decltype(auto) get(const Person& person) {
+  return get_person<Index>(person);
+}
+
+TEST(structured_bindings, enable_in_class_ref) {
+  Person person{"Chuck", "Norris", 81};
+  auto& [name, surname, age] = person;
+  name = "Lando";
+  age = 22;
+  ASSERT_EQ(person.getAge(), 22);
+  ASSERT_EQ(person.getName(), "Lando");
+}
+
+TEST(structured_bindings, enable_in_class_value) {
+  Person person{"Chuck", "Norris", 81};
+  auto [name, surname, age] = person;
+  name = "Lando";
+  age = 22;
+  ASSERT_EQ(person.getAge(), 81);
+  ASSERT_EQ(person.getName(), "Chuck");
+}
--- 17/structured_bindings/structured_bindings.cpp
+++ 17/structured_bindings/structured_bindings.cpp
@@ -1,5 +1,9 @@
+#include <array>
+
 #include "gtest/gtest.h"
 
+// Create structured binding to struct members
+// use assignment initialization
 TEST(structured_bindings, structure) {
   struct Point {
     int x;
@@ -12,4 +16,73 @@ TEST(structured_bindings, structure) {
 
   ASSERT_EQ(x_coord, 1);
   ASSERT_EQ(y_coord, 2);
+}
+
+// Create structured binding to array elements
+TEST(structured_bindings, array) {
+  std::array<int, 3> foo{0, 1, 2};
+  auto& [el0, el1, el2] = foo;
+  el1 = 5;
+  ASSERT_EQ(foo.at(1), 5);
+}
+
+// Init structured binding using brace initialization
+TEST(structured_bindings, brace_init) {
+  struct Point {
+    int x;
+    int y;
+  };
+
+  Point p{1, 2};
+
+  auto [x_coord, y_coord]{p};
+
+  ASSERT_EQ(x_coord, 1);
+  ASSERT_EQ(y_coord, 2);
+}
+
+// Init structured binding using parenthesis constructor
+TEST(structured_bindings, parenthesis_init) {
+  struct Point {
+    int x;
+    int y;
+  };
+
+  Point p{1, 2};
+
+  auto [x_coord, y_coord](p);
+
+  ASSERT_EQ(x_coord, 1);
+  ASSERT_EQ(y_coord, 2);
+}
+
+// Check the underlying type of the binding
+// It shows that there is an entry that is created and
+// actual bindings are members of that entry. If refernce is used
+// the entry itself is a reference and not its members
+TEST(structured_binding, underlying_type) {
+  struct TypeWithValues {
+    TypeWithValues(std::pair<int, int> int_pair)
+        : first{int_pair.first}, second{int_pair.second} {}
+
+    int first;
+    int second;
+  };
+
+  struct TypeWithReferences {
+    TypeWithReferences(std::pair<int, int>& int_pair)
+        : first{int_pair.first}, second{int_pair.second} {}
+    int& first;
+    int& second;
+  };
+
+  std::pair<int, int> int_pair{1, 2};
+  TypeWithValues type_with_values{int_pair};
+  TypeWithReferences type_with_references{int_pair};
+
+  auto& [first, second] = int_pair;
+  ASSERT_FALSE((std::is_same<decltype(type_with_references.first),
+                             decltype(first)>::value));
+  ASSERT_TRUE(
+      (std::is_same<decltype(type_with_values.first), decltype(first)>::value));
 }
