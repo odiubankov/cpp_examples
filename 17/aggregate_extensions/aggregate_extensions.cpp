35239d4aa1cefeea601fc5e4e5ba9cc92b31730e
Oleksii Diubankov <oleksii.diubankov@gmail.com>
2025-08-08 08:29:03 +0000

    Aggregate extension improvements. Add comment. Improve test.

--- 17/aggregate_extensions/aggregate_extensions.cpp
+++ 17/aggregate_extensions/aggregate_extensions.cpp
@@ -1,5 +1,9 @@
 #include "gtest/gtest.h"
 
+// It was not possible to use aggregate initialization until C++17 if class has a base class. 
+
+namespace {
+
 struct A {
   int a_ = 0;
 };
@@ -18,4 +22,6 @@ TEST(aggregate_extension, no_extension_in_construct) {
   B b{1, 2};
   ASSERT_EQ(1, b.a_);
   ASSERT_EQ(2, b.b_);
+}
+
 }
